9ecf722805700d2e148a4afdbdc4f6d5cf28eccb
nobe0716 <nobe0716@gmail.com>
2015-02-26 22:39:47 +0900

    init

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 __author__ = 'nobe0716'
-PYTHON_DIRECTORY_PATH='C:\\10_Linux\\linux-2.6.32.9\\linux-2.6.32.9\\drivers\\'
+#PYTHON_DIRECTORY_PATH='C:\\10_Linux\\linux-2.6.32.9\\linux-2.6.32.9\\drivers\\clocksource'
+#PYTHON_DIRECTORY_PATH='C:\\10_Linux\\linux-2.6.32.9\\linux-2.6.32.9\\drivers\\'
 
+
+PYTHON_DIRECTORY_PATH='C:\\10_Linux\\linux-3.19\\linux-3.19'
 import os
 import re
 
@@ -15,25 +18,25 @@ def checker(file):
 
     try:
         lines = f.readlines()
+        # print ('read lines ' + file)
     except:
-        print ( 'exception occurs readline @ ' + file )
+        #print ( 'exception occurs readline @ ' + file )
+        return None
     for line_idx in range(0, len(lines)):
         line = lines[line_idx]
-        if re.match('void __init .*_init.*', line):
-            pos_init = line_idx
-           # print('init ' + file + str(line_idx))
-        elif re.match('.*setup_irq.*', line):
-            pos_setup_irq = line_idx
-            #print(file + str(line_idx))
-        elif re.match('.*register\(.*', line):
-            pos_register = line_idx
-            #print(file + str(line_idx))
 
-    if pos_init > 0 and pos_setup_irq > 0 and pos_register > 0:
-        if pos_init < pos_setup_irq and pos_setup_irq < pos_register:
-            print (file + "\t====(%d, %d, %d" % pos_init % pos_setup_irq % pos_register)
-        else:
-            print ( 'no bugs at ' + target_name )
+
+        if re.match('^.*setup_irq.*;$', line):
+            pos_setup_irq = line_idx + 1
+        elif re.match('^.*_register\(.*;$', line):
+            pos_register = line_idx + 1
+
+    if pos_setup_irq > 0 and pos_register > 0:
+        if pos_setup_irq < pos_register:
+
+            print ("[%-20s\t====[ %d, %d ]" % (os.path.basename(file), pos_setup_irq, pos_register) )
+
+    f.close();
 
 for r, d, f in os.walk(PYTHON_DIRECTORY_PATH):
     for file in f:
